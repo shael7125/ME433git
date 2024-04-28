7d7e261823b017298a30c3bb022128d3a957e4b1
shael7125 <shaelmurthy@gmail.com>
2024-04-28 14:45:29 -0500

    HW9

---
+++ HW9/uart_advanced.c
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
+#include "hardware/irq.h"
+#include "pico/binary_info.h"
+
+/// \tag::uart_advanced[]
+
+#define UART_ID uart0
+#define BAUD_RATE 115200
+#define DATA_BITS 8
+#define STOP_BITS 1
+#define PARITY    UART_PARITY_NONE
+
+// We are using pins 0 and 1, but see the GPIO function select table in the
+// datasheet for information on which other pins can be used.
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
+volatile int i = 0;
+volatile char m[100];
+
+// RX interrupt handler
+void on_uart_rx() {
+    while (uart_is_readable(UART_ID)) {
+        uint8_t ch = uart_getc(UART_ID);
+        if (ch == '\n') {
+            m[i] = 0;
+            i = 0;
+            printf("From Zero: %s\n",m);
+        } else {
+            m[i] = ch;
+            i++;
+        }
+        // Can we send it back?
+        // if (uart_is_writable(UART_ID)) {
+        //     // Change it slightly first!
+        //     ch++;
+        //     uart_putc(UART_ID, ch);
+        // }
+        // chars_rxed++;
+    }
+}
+
+int main() {
+    stdio_init_all();
+    
+    // initialize PICO LED
+    #ifndef PICO_DEFAULT_LED_PIN
+    #warning blink example requires a board with a regular LED
+    #else
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    // Set up our UART with a basic baud rate.
+    uart_init(UART_ID, 2400);
+
+    // Set the TX and RX pins by using the function select on the GPIO
+    // Set datasheet for more information on function select
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+    // Actually, we want a different speed
+    // The call will return the actual baud rate selected, which will be as close as
+    // possible to that requested
+    int __unused actual = uart_set_baudrate(UART_ID, BAUD_RATE);
+
+    // Set UART flow control CTS/RTS, we don't want these, so turn them off
+    uart_set_hw_flow(UART_ID, false, false);
+
+    // Set our data format
+    uart_set_format(UART_ID, DATA_BITS, STOP_BITS, PARITY);
+
+    // Turn off FIFO's - we want to do this character by character
+    uart_set_fifo_enabled(UART_ID, false);
+
+    // Set up a RX interrupt
+    // We need to set up the handler first
+    // Select correct interrupt for the UART we are using
+    int UART_IRQ = UART_ID == uart0 ? UART0_IRQ : UART1_IRQ;
+
+    // And set up and enable the interrupt handlers
+    irq_set_exclusive_handler(UART_IRQ, on_uart_rx);
+    irq_set_enabled(UART_IRQ, true);
+
+    // Now enable the UART to send interrupts - RX only
+    uart_set_irq_enables(UART_ID, true, false);
+
+    // OK, all set up.
+    // Lets send a basic string out, and then run a loop and wait for RX interrupts
+    // The handler will count them, but also reflect the incoming data back with a slight change!
+    uart_puts(UART_ID, "\nHello, uart interrupts\n");
+
+    while (1) {
+        // heartbeat at 1 Hz
+        gpio_put(LED_PIN, 1);
+        
+        int ifromcomp = 0;
+        scanf("%d",&ifromcomp);
+        printf("Comp sent : %d\n", ifromcomp);
+
+        char txm[100];
+        sprintf(txm, "%d\n", ifromcomp);
+        uart_puts(UART_ID, txm);
+
+        sleep_ms(500);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(500);
+        // heartbeat at 1 Hz
+    }
+    #endif
+}
+// / \end:uart_advanced[]
